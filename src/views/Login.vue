544720c1a05ee0e91d79b9f8ab27e0ee4d0ad1ee
SamuelTan <guo.yang.1215@hotmail.com>
2023-12-27 11:59:42 +0800

    1.整理HTML結構 2.script 宣告token 及 expired ，將token / expired 組成cookie

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,36 +1,36 @@
 <template>
     <div class="container mt-5">
         <form class="row justify-content-center" @submit.prevent="signIn">
-        <div class="col-md-6">
-            <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
-            <div class="mb-2">
-            <label for="inputEmail" class="sr-only">Email address</label>
-            <input
-                type="email"
-                id="inputEmail"
-                class="form-control"
-                placeholder="Email address"
-                required
-                autofocus
-                v-model="user.username"
-            />
+            <div class="col-md-6">
+                <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+                <div class="mb-2">
+                    <label for="inputEmail" class="sr-only">Email address</label>
+                    <input
+                        type="email"
+                        id="inputEmail"
+                        class="form-control"
+                        placeholder="Email address"
+                        required
+                        autofocus
+                        v-model="user.username"
+                    />
+                </div>
+                <div class="mb-2">
+                    <label for="inputPassword" class="sr-only">Password</label>
+                    <input
+                        type="password"
+                        id="inputPassword"
+                        class="form-control"
+                        placeholder="Password"
+                        required
+                        v-model="user.password"
+                    />
+                </div>
+            
+                <div class="text-end mt-4">
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+                </div>
             </div>
-            <div class="mb-2">
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input
-                type="password"
-                id="inputPassword"
-                class="form-control"
-                placeholder="Password"
-                required
-                v-model="user.password"
-            />
-            </div>
-        
-            <div class="text-end mt-4">
-            <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
-            </div>
-        </div>
         </form>
     </div>
 </template>
@@ -54,6 +54,10 @@ export default {
             console.log(api);
             this.$http.post(api ,this.user)
             .then((res)=>{
+                const token = res.data.token;
+                const expired = res.data.expired;
+                console.log(token, expired);
+                document.cookie = `hexToken=${token};expires=${new Date(expired)};`;
                 console.log(res);
             })
         }
