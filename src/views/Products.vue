e5e3657fa549d8f695f4daf5c8d77b66acb079fb
SamuelTan <guo.yang.1215@hotmail.com>
2023-12-29 12:31:15 +0800

    在Products頁面加入 BTN新增產品 來彈跳出ProductModal

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,47 +1,55 @@
 <template>
-<table class="table mt-4">
-  <thead>
-    <tr>
-      <th width="120">分類</th>
-      <th>產品名稱</th>
-      <th width="120">原價</th>
-      <th width="120">售價</th>
-      <th width="100">是否啟用</th>
-      <th width="200">編輯</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="item in products" :key="item.id">
-      <td>{{item.category}}</td>
-      <td>{{item.title}}</td>
-      <td class="text-right">
-        {{item.origin_price}}
-      </td>
-      <td class="text-right">
-        {{item.price}}
-      </td>
-      <td>
-        <span class="text-success" v-if="item.is_enabled">啟用</span>
-        <span class="text-muted" v-else>啟用</span>
-      </td>
-      <td>
-        <div class="btn-group">
-          <button class="btn btn-outline-primary btn-sm">編輯</button>
-          <button class="btn btn-outline-danger btn-sm">刪除</button>
-        </div>
-      </td>
-    </tr>
-  </tbody>
-</table>
+  <div class="text-end">
+    <button class="btn btn-primary" type="button" @click="$refs.productModal.showModal">增加一個產品</button>
+  </div>
+  <table class="table mt-4">
+    <thead>
+      <tr>
+        <th width="120">分類</th>
+        <th>產品名稱</th>
+        <th width="120">原價</th>
+        <th width="120">售價</th>
+        <th width="100">是否啟用</th>
+        <th width="200">編輯</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in products" :key="item.id">
+        <td>{{item.category}}</td>
+        <td>{{item.title}}</td>
+        <td class="text-right">
+          {{item.origin_price}}
+        </td>
+        <td class="text-right">
+          {{item.price}}
+        </td>
+        <td>
+          <span class="text-success" v-if="item.is_enabled">啟用</span>
+          <span class="text-muted" v-else>未啟用</span>
+        </td>
+        <td>
+          <div class="btn-group">
+            <button class="btn btn-outline-primary btn-sm">編輯</button>
+            <button class="btn btn-outline-danger btn-sm">刪除</button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <ProductModal ref="productModal"></ProductModal>
 </template>
 
 <script>
+import ProductModal from '../components/ProductModal.vue'
 export default{
   data(){
     return {
       products:[],
       pagination:{},
     }
+  },
+  components:{
+    ProductModal,
   },
   // bug 沒有取得res，axios 設定錯誤？
   // 取得產品列表（產品資訊，分頁）
@@ -51,7 +59,6 @@ export default{
       this.$http.get(api)
       .then((res)=>{
         if(res.data.success){
-          console.log(res.data);
           this.products = res.data.products;
           this.pagination = res.data.pagination;
         }
